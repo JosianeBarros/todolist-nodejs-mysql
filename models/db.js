8cc0b9b50536c359e76850603dbf55cac03ad8f3
Josiane <108816336+JosianeBarros@users.noreply.github.com>
2023-06-23 23:10:41 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('todolist', 'root', '220103', {
+const sequelize = new Sequelize('todolist', 'root', '012145', {
     host: 'localhost',
     dialect: 'mysql',
     query:{raw: true}
